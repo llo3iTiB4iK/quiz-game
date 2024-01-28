496d8eca6cd87541c4391a53df5b027c2d4e3d3e
llo3iTiB4iK <90177277+llo3iTiB4iK@users.noreply.github.com>
2024-01-28 13:52:44 +0200

    Add files via upload
    Now user can choose type of questions (true/false or multiple choice), fixed bug with displaying symbols ' and `

--- quiz_brain.py
+++ quiz_brain.py
@@ -7,8 +7,8 @@ class QuizBrain:
     def next_question(self):
         current_question = self.questions_list[self.question_number]
         self.question_number += 1
-        user_answer = input(f"Q.{self.question_number}: {current_question.text} (True/False)?: ")
-        self.check_answer(user_answer, current_question.answer)
+        user_answer = input(f"Q.{self.question_number}: {current_question.text} ({current_question.get_answers()})?: ")
+        self.check_answer(user_answer, current_question.correct_answer)
 
     def still_has_questions(self):
         return self.question_number < len(self.questions_list)
