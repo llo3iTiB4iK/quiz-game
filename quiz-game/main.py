2922ad96950a5999c9118c47ce7658d1d95cee07
llo3iTiB4iK <90177277+llo3iTiB4iK@users.noreply.github.com>
2024-02-09 10:55:04 +0200

    Delete quiz-game directory

--- quiz-game/main.py
+++
@@ -1,8 +0,0 @@
-from question_model import Question
-from quiz_brain import QuizBrain
-from ui import QuizGUI
-from data import question_data
-
-questions = [Question(q['question'], q['correct_answer'], q['incorrect_answers'], q['type']) for q in question_data]
-quiz = QuizBrain(questions)
-quiz_ui = QuizGUI(quiz)
